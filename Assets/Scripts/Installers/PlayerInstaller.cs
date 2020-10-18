f67d585cd50a17bb92578ff199c7897f456a2de7
Heverton Sarah <heverton.sarah@gmail.com>
2020-10-17 21:37:11 -0300

    Dispose bullets after defined time
    See: #3

--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -6,14 +6,14 @@ namespace AsteroidsGame.Installer
 {
     public class PlayerInstaller : MonoInstaller
     {
-
+        private const int BULLETS_INITIAL_AMOUNT = 15;
         [SerializeField]
         private GameObject bulletPrefab;
 
         public override void InstallBindings()
         {
             Container.BindFactory<Vector2, Vector2, BulletComponent, BulletComponent.Factory>().FromMonoPoolableMemoryPool<Vector2, Vector2, BulletComponent>(
-                x => x.WithInitialSize(2).FromComponentInNewPrefab(bulletPrefab).UnderTransformGroup("BulletPool"));
+                x => x.WithInitialSize(BULLETS_INITIAL_AMOUNT).FromComponentInNewPrefab(bulletPrefab).UnderTransformGroup("BulletPool"));
         }
     }
 }
--- Assets/Scripts/View/BulletComponent.cs
+++ Assets/Scripts/View/BulletComponent.cs
@@ -8,29 +8,41 @@ namespace AsteroidsGame.View
 {
     public class BulletComponent : MonoBehaviour, IPoolable<Vector2, Vector2, IMemoryPool>, IDisposable
     {
+        private const float SECONDS_BEFORE_INVOKING = 3.0f;
+        private const string AUTO_DESTRUCTION_FUNCION = "Dispose";
+
         private IMemoryPool pool;
+        private Vector2 initialPosition;
 
         [SerializeField]
         private Rigidbody2D objectRigidbody;
         [SerializeField]
         private ForceBasedMovementComponent movementComponent;
 
-        public void OnSpawned(Vector2 initialPosition, Vector2 direction, IMemoryPool pool)
+        private void Start()
+        {
+            initialPosition = transform.position;
+        }
+
+        public void OnSpawned(Vector2 spawnPosition, Vector2 direction, IMemoryPool pool)
         {
             this.pool = pool;
-            transform.position = initialPosition;
+            transform.position = spawnPosition;
             transform.up = direction;
             movementComponent.OnMoveInput();
+            Invoke(AUTO_DESTRUCTION_FUNCION, SECONDS_BEFORE_INVOKING);
+            
         }
 
         public void Dispose()
         {
-            
+            pool.Despawn(this);
         }
 
         public void OnDespawned()
         {
-            
+            CancelInvoke(AUTO_DESTRUCTION_FUNCION);
+            transform.position = initialPosition;
         }
 
         public class Factory : PlaceholderFactory<Vector2, Vector2, BulletComponent>
