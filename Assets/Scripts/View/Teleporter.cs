3751f0be3f1cc366f12df42cdc48027fd75cefcf
Heverton Sarah <heverton.sarah@gmail.com>
2020-10-18 23:24:19 -0300

    Teleport gameobject when leaves screen
    The game object is teleported to the oposite side of where it leaves the
    screen.
    
    See: #5

---
+++ Assets/Scripts/View/Teleporter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace AsteroidsGame.View
+{
+    public class Teleporter : MonoBehaviour
+    {
+        private Vector3 currentPosition;
+        private Vector2 minimumPositionInWorldSpace;
+        private Vector2 maximumPositionInWorldSpace;
+        private CameraStatsRetriever cameraStatsRetriever;
+
+        [Inject]
+        private void Construct(CameraStatsRetriever cameraStatsRetriever)
+        {
+            this.cameraStatsRetriever = cameraStatsRetriever;
+        }
+
+        private void Start()
+        {
+            SetupMaximumPosition();
+            SetupMinimumPosition();
+        }
+
+        public void OnLeaveScreen()
+        {
+            TeleportToOpositeSide();
+        }
+
+        private void TeleportToOpositeSide()
+        {
+            currentPosition = transform.position;
+
+            if (IsObjectOutOfHorizontalBounds())
+                currentPosition.x = -currentPosition.x;
+            if (IsObjectOutOfVerticalBounds())
+                currentPosition.y = -currentPosition.y;
+
+            transform.position = currentPosition;
+        }
+
+        private bool IsObjectOutOfHorizontalBounds()
+        {
+            return currentPosition.x > maximumPositionInWorldSpace.x || currentPosition.x < minimumPositionInWorldSpace.x;
+        }
+
+        private bool IsObjectOutOfVerticalBounds()
+        {
+            return currentPosition.y > maximumPositionInWorldSpace.y || currentPosition.y < minimumPositionInWorldSpace.y;
+        }
+
+        private void SetupMaximumPosition()
+        {
+            float maximumViewportValue = cameraStatsRetriever.ViewPortMaximumValue;
+
+            maximumPositionInWorldSpace =  cameraStatsRetriever.ConvertToWorldPosition(new Vector2(maximumViewportValue, maximumViewportValue));
+        }
+
+        private void SetupMinimumPosition()
+        {
+            float minimumViewportValue = cameraStatsRetriever.ViewPortMinimumValue;
+
+            minimumPositionInWorldSpace =  cameraStatsRetriever.ConvertToWorldPosition(new Vector2(minimumViewportValue, minimumViewportValue));
+        }
+    }
+}
---
+++ Assets/Scripts/View/VisibilityChecker.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Zenject;
+
+namespace AsteroidsGame.View
+{
+    public class VisibilityChecker : MonoBehaviour
+    {
+        private Camera gameCamera;
+        private Plane[] planes;
+        private bool isVisible;
+
+        [SerializeField]
+        private Collider2D objectCollider;
+        [SerializeField]
+        private UnityEvent OnEnterScreen, OnLeaveScreen;
+
+        [Inject]
+        private void Construct(Camera gameCamera)
+        {
+            this.gameCamera = gameCamera;
+        }
+
+        void Start()
+        {
+            planes = GeometryUtility.CalculateFrustumPlanes(gameCamera);
+        }
+
+        void Update()
+        {
+            if (HasObjectEnteredScreen())
+            {
+                isVisible = true;
+                OnEnterScreen?.Invoke();
+            }
+            else if (HasObjectLeftScreen())
+            {
+                isVisible = false;
+                OnLeaveScreen?.Invoke();
+            }
+        }
+
+        private bool HasObjectEnteredScreen()
+        {
+            return IsObjectInsideScreenBounds() && !isVisible;
+        }
+
+        private bool HasObjectLeftScreen()
+        {
+            return !IsObjectInsideScreenBounds() && isVisible;
+        }
+
+        private bool IsObjectInsideScreenBounds()
+        {
+            return GeometryUtility.TestPlanesAABB(planes, objectCollider.bounds);
+        }
+    }
+}
