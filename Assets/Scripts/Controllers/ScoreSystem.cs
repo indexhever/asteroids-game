8ea5fe2bb67e92da49730f124f7ec5da82fa6ce3
Heverton Sarah <heverton.sarah@gmail.com>
2020-10-19 19:53:41 -0300

    Control score adding and removing
    When adding or removing score will also updates its visual UI
    
    See: #6

---
+++ Assets/Scripts/Controllers/ScoreSystem.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System;
+using AsteroidsGame.View;
+
+namespace AsteroidsGame.Controllers
+{
+    public class ScoreSystem
+    {
+        private ScoreVisual scoreVisual;
+
+        public ScoreSystem(ScoreVisual scoreVisual)
+        {
+            this.scoreVisual = scoreVisual;
+        }
+
+        public int TotalScore { get; private set; }
+
+        public void Add(int pointsToAdd)
+        {
+            TotalScore += pointsToAdd;
+            UpdateVisual();
+        }
+
+        public void Remove(int pointsToRemove)
+        {
+            TotalScore -= pointsToRemove;
+            UpdateVisual();
+        }
+
+        private void UpdateVisual()
+        {
+            scoreVisual.UpdateWithNewScore(TotalScore);
+        }
+    }
+}
