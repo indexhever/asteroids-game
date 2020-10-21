19b558bfbd74a3d24191ba6b3f57a44d21a27cb1
Heverton Sarah <heverton.sarah@gmail.com>
2020-10-21 08:35:49 -0300

    Setup enemy spaceship visual and basic components
    See: #4

---
+++ Assets/Scripts/View/EnemySpaceShipComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace AsteroidsGame.View
+{
+    public class EnemySpaceShipComponent : MonoBehaviour, IDisposable
+    {
+        [SerializeField]
+        private UnityEvent OnDie;
+
+        public void Dispose()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
