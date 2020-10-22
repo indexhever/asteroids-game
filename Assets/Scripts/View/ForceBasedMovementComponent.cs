6288b82a0ae35e19a1ec41783f8b5a72109f0cce
Heverton Sarah <heverton.sarah@gmail.com>
2020-10-22 13:57:06 -0300

    Remove speed control related code from Movement
    It will separate speed related control code from the Movement related
    control to only use speed control when really necessary.
    
    See: #23

--- Assets/Scripts/View/ForceBasedMovementComponent.cs
+++ Assets/Scripts/View/ForceBasedMovementComponent.cs
@@ -10,15 +10,11 @@ namespace AsteroidsGame.View
     public class ForceBasedMovementComponent : MonoBehaviour
     {
         private MovementListener movementListener;
-        private Vector2 currentVelocity;
-        private Vector2 newVelocity;
 
         [SerializeField]
         private Rigidbody2D objectRigidbody;
         [SerializeField]
         private float thrust; // 50 is 1 unit per second
-        [SerializeField]
-        private float maxSpeed;
 
         [Inject]
         private void Construct(ForceBasedMovementController.Factory movementFactory)
@@ -34,27 +30,6 @@ namespace AsteroidsGame.View
         private void AddForce()
         {
             objectRigidbody.AddForce(transform.up * thrust);
-        }
-
-        public void FixedUpdate()
-        {
-            LimitSpeedToMaximumAllowed();
-        }
-
-        private void LimitSpeedToMaximumAllowed()
-        {
-            currentVelocity = objectRigidbody.velocity;
-            if (!HasAchievedMaximumSpeed())
-                return;
-
-            newVelocity = currentVelocity.normalized;
-            newVelocity *= maxSpeed;
-            objectRigidbody.velocity = newVelocity;
-        }
-
-        private bool HasAchievedMaximumSpeed()
-        {
-            return Mathf.Abs(currentVelocity.x) > maxSpeed || Mathf.Abs(currentVelocity.y) > maxSpeed;
         }
     }
 }
---
+++ Assets/Scripts/View/SpeedLimiterComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsteroidsGame.View
+{
+    public class SpeedLimiterComponent : MonoBehaviour
+    {
+        private Vector2 currentVelocity;
+        private Vector2 newVelocity;
+
+        [SerializeField]
+        private float maxSpeed;
+
+        [SerializeField]
+        private Rigidbody2D objectRigidbody;
+
+        public void FixedUpdate()
+        {
+            LimitSpeedToMaximumAllowed();
+        }
+
+        private void LimitSpeedToMaximumAllowed()
+        {
+            currentVelocity = objectRigidbody.velocity;
+            if (!HasAchievedMaximumSpeed())
+                return;
+
+            newVelocity = currentVelocity.normalized;
+            newVelocity *= maxSpeed;
+            objectRigidbody.velocity = newVelocity;
+        }
+
+        private bool HasAchievedMaximumSpeed()
+        {
+            return Mathf.Abs(currentVelocity.x) > maxSpeed || Mathf.Abs(currentVelocity.y) > maxSpeed;
+        }
+    }
+}
